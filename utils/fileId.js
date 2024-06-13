27a152f0546caafb37aa977c408c108f515ae65a
zabilsabri <zabilsabrimuhammad0@gmail.com>
2024-06-14 00:17:31 +0800

    `feat`: add fileId & imageKit utils

---
+++ utils/fileId.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+require('dotenv').config();
+
+async function getFileId(name) {
+    try {
+        const imageKitUrl = `https://api.imagekit.io/v1/files?searchQuery=name = "${name}"`;
+        
+        const response = await axios.get(imageKitUrl, {
+          headers: {
+            'Authorization': `Basic ${Buffer.from(`${process.env.IMAGEKIT_PRIVATE_KEY}:`).toString('base64')}`,
+        }
+        });
+    
+        if (response.status === 200) {
+          const fileId = response.data[0].fileId;
+          return fileId
+        } else {
+          return 'Error fetching fileId:', response.statusText;
+        }
+    } catch (error) {
+        return 'Error fetching fileId:', error;
+    }    
+}
+
+module.exports = getFileId;
---
+++ utils/imageKit.js
@@ -0,0 +1,10 @@
+const ImageKit = require('imagekit');
+require('dotenv').config();
+
+const imagekit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+module.exports = imagekit;
