822f95fd75b1d31185865a29e997a61544184f9b
zabilsabri <zabilsabrimuhammad0@gmail.com>
2024-06-14 00:17:14 +0800

    `fix`: modify multer utils

--- utils/multer.js
+++ utils/multer.js
@@ -1,59 +1,25 @@
 const multer = require('multer');
-const fs = require('fs');
 const path = require('path');
 
-const filename = (req, file, callback) => {
-    let fileName = Date.now() + path.extname(file.originalname);
-    callback(null, fileName);
-};
-
-const generateStorage = (destination) => {
-    return multer.diskStorage({
-        destination: (req, file, callback) => {
-            const dir = './tmp/images';
-
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir, { recursive: true });
-            }
-
-            callback(null, dir);
-        },
-        filename: filename
-    });
-};
-
-const generateFileFilter = (mimetypes) => {
-    return (req, file, callback) => {
-        if (mimetypes.includes(file.mimetype)) {
-            callback(null, true);
-        } else {
-            let err = new Error(`Only ${mimetypes} are allowed to upload!`);
-            callback(err, false);
-        }
-    };
-};
-
 module.exports = {
-    imageStorage: multer({
-        storage: generateStorage('./tmp/images'),
-        fileFilter: generateFileFilter([
-            'image/png',
-            'image/jpg',
-            'image/jpeg'
-        ]),
+    image: multer({
+        fileFilter: (req, file, cb) => {
+            let allowedMimeTypes = [
+                'image/jpeg', 
+                'image/png', 
+                'image/jpg'
+            ];
+            
+            if(allowedMimeTypes.includes(file.mimetype)){
+                cb(null, true);
+            } else {
+                let err = new Error('File type not allowed. Only JPEG, PNG, and JPG');
+                cb(err, false);
+            }
+        },
         onError: (err, next) => {
+            console.log(err);
             next(err);
         }
     }),
-
-    image: multer({
-        fileFilter: generateFileFilter([
-            'image/png',
-            'image/jpg',
-            'image/jpeg'
-        ]),
-        onError: (err, next) => {
-            next(err);
-        }
-    })
-};
+}
